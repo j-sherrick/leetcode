5f421e4fcfa58b7cdaf7aefd5a2345fb831be6fe
Jason Sherrick <jasonsherrick@gmail.com>
2024-08-04 15:54:31 -0500

    tinkered around with longestSubstring

--- medium/longest-substring/longestsub.c
+++ medium/longest-substring/longestsub.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
 int lengthOfLongestSubstring(char* s) {
-    unsigned short hashArr['z' + 1];
-    size_t longestSubstring = 0;
-    size_t thisSubstring = 0;
-    size_t i = 0;
-    hashArr[s[i]] = 1;
-    size_t sLength = sizeof(s) / sizeof(s[0]);
-    for (size_t k = 1; k < sLength; k++) {
-      if (hashArr[s[k]]) {
-        if (thisSubstring > longestSubstring)
-          longestSubstring = thisSubstring;
-        while(i < k) {
-          hashArr[s[i++]] = 0;
-        }
-        thisSubstring = 0;
+    size_t lengthOfLongest = 0, currentLength = 0;
+    char* left = s;
+    char* right = (s + 1);
+    while ( *right != '\0') {
+      if (*left == *right)
+      {
       }
-      else {
-        
-      }
+      
     }
 }
